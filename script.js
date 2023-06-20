660ecb19625bee75532bfa5120b36ff592364b12
sabrinaK <sabrinapsouza@hotmail.com>
2023-06-20 14:24:38 -0400

    complete home work

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event)) {
   // Random quote of the day generator
   const randomQuote = function() {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
@@ -17,32 +17,58 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  const mainTitleEl = document.getElementById('main-title');
+  mainTitleEl.textContent = " ";
+  
 
   // Part 2
-
+const bodyEl = document.querySelector('body');
+bodyEl.style.backgroundColor = 'blue'
 
   // Part 3
-
+  const favoriteThingsList = document.getElementById('favorite-things');
+  const lastListItem = favoriteThingsList.lastElementChild;
+  lastListItem.remove();
 
   // Part 4
-
+const specialTitles = document.querySelectorAll('.special-title');
+specialTitlesElements.forEach(element =>{
+  element.style.fontSize ='2rem'; 
+});
 
   // Part 5
-
+const pastRacesList = document.getElementById('#past-races');
+const chicagoListItem = pastRacesList.querySelector('li');
+chicagoListRace.remove(); 
+}
 
   // Part 6
+const newCity = 'Miami';
+const newLi = document.createElement('li');
+newLi.textContent = newCity;
+pastRacesList.appendChild(newLi);
 
+// part 7 
+const blogPostContainer = document.querySelector('.blog-posts');
+const newBlogPost =document.createElement('div');
+newBlogPost.classList.add('blog-post')
+const newHeading = document.createElement('h2');
+newHeading.textContent = newCity;
+const newParagraph = document.createElement('p') 
+newParagraph.appendChild(newHeading);
+newBlogPost.appendChild(newParagraph);
+blogPostContainer.appendChild(newBlogPost); 
+// Part 8
+const quoteTitleEL = document.querySelector('#quote-title');
+quoteTitleEL.addEventListoner('click', randomQuote);
 
-  // Part 7
-
-
-  // Part 8
-
-
-  // Part 9
-
-
-
-
+// Part 9
+const blogPost = document.querySelectorAll('.blog-post');
+blogPost.forEach(post => {
+  post.addEventListener('mouseout',function(){
+    this.classList.toggle('blue');
+  });
+  post.addEventListener('mouseenter', function(){
+    this.classList.toggle('red');
+  });
 });
